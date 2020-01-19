d636465783437e106280fd91222ab15db546e004
Ciro Nishiguchi <cironishiguchi@gmail.com>
2020-01-18 18:51:25 -0600

    Add note on getting range of commits

--- gitpython/gitpython_usage.py
+++ gitpython/gitpython_usage.py
@@ -11,6 +11,11 @@ print("Active branch: {}".format(active_branch))
 commits = list(repo.iter_commits(max_count=10))
 print("Num commits: {}".format(len(commits)))
 
+# Get commits in a range
+commit_range =  "HEAD~{}..HEAD".format(len(commits)-1)
+other_commits = list(repo.iter_commits(commit_range))
+assert commits[0].hexsha == other_commits[0].hexsha
+
 # Print some info on latest commit
 summary = commits[0].summary
 print("Last commit: {}".format(summary))
@@ -20,7 +25,7 @@ author = commits[0].author
 print("Last commit author: {}".format(author))
 
 # Checkout oldest commit
-repo.git.checkout(str(commits[-1].hexsha))
+#repo.git.checkout(str(commits[-1].hexsha))
 
 # Checkout master branch
-repo.git.checkout("master")
+#repo.git.checkout("master")
